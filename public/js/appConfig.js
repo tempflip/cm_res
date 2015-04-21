c30f8cdc48ffdc91f9d4cdf576809c6656b4db36
Peter <Peter@192.168.1.2>
2015-04-21 10:00:56 +0100

    public login

--- public/js/appConfig.js
+++ public/js/appConfig.js
@@ -4,6 +4,12 @@ var UPDATE_TASK_URL = 'http://localhost:8000/updateTask';
 var SUBMIT_TASK_URL = 'http://localhost:8000/submitTask';
 var REWARDS_URL = 'http://localhost:8000/rewards';
 
+var FB_APP_ID = '485645464921474';
+
+if (document.domain == 'localhost') {
+	FB_APP_ID = '1584826841759329';
+}
+
 var levelLabel = {
 	0 : 'Könnyű',
 	1 : 'Közepes',
