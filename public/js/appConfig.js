7bf6fa887a576b399eae58219b98d1c57dda0fc8
Peter <Peter@192.168.1.2>
2015-04-21 10:05:31 +0100

    public login

--- public/js/appConfig.js
+++ public/js/appConfig.js
@@ -1,13 +1,18 @@
-var LOGIN_URL = 'http://localhost:8000/login';
-var TASKS_URL = 'http://localhost:8000/tasks';
-var UPDATE_TASK_URL = 'http://localhost:8000/updateTask';
-var SUBMIT_TASK_URL = 'http://localhost:8000/submitTask';
-var REWARDS_URL = 'http://localhost:8000/rewards';
+var LOGIN_URL = 'http://geekwerk.herokuapp.com/login';
+var TASKS_URL = 'http://geekwerk.herokuapp.com/tasks';
+var UPDATE_TASK_URL = 'http://geekwerk.herokuapp.com/updateTask';
+var SUBMIT_TASK_URL = 'http://geekwerk.herokuapp.com/submitTask';
+var REWARDS_URL = 'http://geekwerk.herokuapp.com/rewards';
 
 var FB_APP_ID = '485645464921474';
 
 if (document.domain == 'localhost') {
 	FB_APP_ID = '1584826841759329';
+	var LOGIN_URL = 'http://localhost:8000/login';
+	var TASKS_URL = 'http://localhost:8000/tasks';
+	var UPDATE_TASK_URL = 'http://localhost:8000/updateTask';
+	var SUBMIT_TASK_URL = 'http://localhost:8000/submitTask';
+	var REWARDS_URL = 'http://localhost:8000/rewards';
 }
 
 var levelLabel = {
